8a50a45357d00ac347ee70e01010c8e45ce3205f
abdarwishAI <abdarwish.ai@gmail.com>
2022-12-14 20:45:30 +0900

    done

--- Infra_WebUI.py
+++ Infra_WebUI.py
@@ -24,19 +24,20 @@ import streamlit as st
 hide_menu_style = """
         <style>
         #MainMenu {visibility: hidden;}
+        #footer {visibility: hidden;}
         </style>
         """
 st.markdown(hide_menu_style, unsafe_allow_html=True)
 
 #----------------------Hide Streamlit footer----------------------------
-hide_st_style = """
+##hide_st_style = """
 
 #MainMenu {visibility: hidden;}
-footer {visibility: hidden;}
-header {visibility: hidden;}
+##footer {visibility: hidden;}
+##header {visibility: hidden;}
 
-"""
-st.markdown(hide_st_style, unsafe_allow_html=True)
+##"""
+#st.markdown(hide_st_style, unsafe_allow_html=True)
 #--------------------------------------------------------------------
 
 # import streamlit
